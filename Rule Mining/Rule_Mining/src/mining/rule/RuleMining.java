9d4cd04b06eb166e20f6e699db4b0af5c8214631
greetsandeep <greetsandeep@gmail.com>
2017-04-02 14:41:25 +0530

    I think we are done with pre-processing

--- Rule Mining/Rule_Mining/src/mining/rule/RuleMining.java
+++ Rule Mining/Rule_Mining/src/mining/rule/RuleMining.java
@@ -21,17 +21,21 @@ public class RuleMining {
 		}catch(Exception e){
 			System.out.println("Error In file Reading " + e);
 		}
-		for(int i = 0;i<17;i++)
-		{
-			int count = 0;
-			for(int j = 0;j<data.size();j++)
-			{
-				int temp[] = data.get(j);
-				if(temp[i]==-1)
-					count++;
-			}
-			System.out.println(i+" "+count);
-		}
+		
+		
+//		for(int i = 0;i<17;i++)
+//		{
+//			int count = 0;
+//			for(int j = 0;j<data.size();j++)
+//			{
+//				int temp[] = data.get(j);
+//				if(temp[i]==-1)
+//					count++;
+//			}
+//			System.out.println(i+" "+count);
+//		}
+		
+		
 		handleMissingValues(data);
 	}
 
@@ -44,11 +48,7 @@ public class RuleMining {
 	public static void inputHandle(String filename,ArrayList<int[]> data)throws IOException {
 		BufferedReader br = new BufferedReader(new FileReader(filename));
 		String line=null;
-		int flag = 1;
-		int lineNum = 0;
 		while( (line=br.readLine()) != null) {
-			flag = 1;
-			lineNum++;
 			StringTokenizer st = new StringTokenizer(line,",");
 			int row[] = new int[17];
 			while(st.hasMoreTokens()){
@@ -57,12 +57,8 @@ public class RuleMining {
 					row[i] = getInt(st.nextToken());
 				}
 			}
-			for(int i=0;i<row.length;i++)
-			{
-				if(row.length!=17)
-					System.out.println(lineNum);
-			}
-			data.add(row);
+			
+			data.add(expand(row));
 		}       
 		br.close();
 	}
@@ -89,5 +85,41 @@ public class RuleMining {
 	 * This function replaces the '?' i.e. the missing values, with suitable values
 	 */
 	public static void handleMissingValues(ArrayList<int[]> data){
+	}
+	
+	/**
+	 * @param row the row which we created after reading in the data
+	 * @return a row with binarized categorical data. The attribute corresponding to the data can be found in DataRef Class
+	 */
+	public static int[] expand(int row[]){
+		int ans[] = new int[33];
+		int j = 0;
+		for(int i=0;i<row.length-1;i++)
+		{
+			if(row[i]==1)
+			{
+				ans[j]   = 0;
+				ans[j+1] = 1;
+			}else if(row[i]==0){
+				ans[j]   = 1;
+				ans[j+1] = 0;
+			}
+			j = j+2;
+		}
+		ans[32] = row[row.length-1];
+		//printArray(row);
+		//printArray(ans);
+		System.out.println("*******");
+		return ans;
+	}
+	
+	/**
+	 * @param row the row which we want to print
+	 * An utility function that prints the passed array
+	 */
+	public static void printArray(int []row){
+		for(int i=0;i<row.length;i++)
+			System.out.print(row[i]+" ");
+		System.out.println();
 	}
 }
