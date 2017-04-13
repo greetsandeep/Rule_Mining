0b0a8f69a457e4192ba9effbbf1ccdf0f57d6b31
snehalw7 <snehal.wadhwani7@gmail.com>
2017-04-14 01:08:28 +0530

    Hash Tree documentation

--- Rule Mining/Rule_Mining/src/mining/rule/HashTree.java
+++ Rule Mining/Rule_Mining/src/mining/rule/HashTree.java
@@ -2,13 +2,31 @@ package mining.rule;
 
 import java.util.*;
 
+/**
+ * @author Snehal, Sandeep, Poojitha
+ * This class implements the structure of a HashTree and all the methods associated with it. 
+ */
 public class HashTree {
+	/** Indicates the depth of this HashTree Node. */
 	int depth;
+	
+	/** A list that stores the hashed itemsets if this node acts as leaf. */
 	ArrayList<TreeSet<Integer>> candidate;
+	
+	/** A list that stores the support count of the corresponding candidate itemsets stored in the ArrayList candidate */
 	ArrayList<Integer> supportCount;
+	
+	/** A list that stores the object references to the children based on the hash value if this node acts as an internal node.  */
 	ArrayList<HashTree> children;
+	
+	/** The value of the hash function  */
 	int hash;
 	
+	/**
+	 * @param hash The hash value to be used by the tree 
+	 * @param depth Depth of the the node being created 
+	 * Constructor assigns appropriate initial values to the class attributes with customized hash and depth values. 
+	 */
 	HashTree(int hash, int depth){
 		this.hash = hash;
 		this.depth = depth;
@@ -22,6 +40,9 @@ public class HashTree {
 		}
 	}
 		
+	/**
+	 * @param set The itemset that is to be hashed into the Hash Tree
+	 */
 	public void hashItemset(TreeSet<Integer> set){
 			HashTree node = this;
 			TreeSet<Integer> copy = new TreeSet<Integer>(set);
@@ -54,7 +75,6 @@ public class HashTree {
 								toCopy.clear();
 								toCopy.addAll(node.candidate.get(i));
 								if(toCopy.size()>0){
-									//System.out.println(toCopy);
 									for(int j=0;j<node.depth;j++){
 										toCopy.remove(toCopy.first());
 									}								
@@ -75,6 +95,9 @@ public class HashTree {
 		
 	}
 	
+	/**
+	 * @param itemset Itemset whose support count is to be increased, if it exists in the Hash Tree
+	 */
 	public void updateSupportCount(ArrayList<Integer> itemset){
 		HashTree node = this;
 		int flag = 0;
@@ -118,6 +141,11 @@ public class HashTree {
 		}
 	}
 	
+	/**
+	 * @param minsup Support threshold 
+	 * @param noOfTransactions Total number of transactions
+	 * @return boolean value to indicate whether there were any subsets above the support threshold. Returns true if the Set of itemsets were updated and false otherwise. 
+	 */
 	public boolean updateItemsets(double minsup, int noOfTransactions){
 		boolean status = false;
 		RuleMining.itemsets.get(15-hash).clear();
@@ -146,29 +174,7 @@ public class HashTree {
 				}
 			}
 		}
-		
-		/*if(node.candidate == null){
-			for(int i=0;i<hash;i++){
-				if(node.children.get(i)!=null){
-					node.children.get(i).updateItemsets(minsup,noOfTransactions);
-				}
-			}
-		}
-		else{
-			for(int i = 0;i<node.candidate.size();i++){
-				//System.out.println(node.supportCount.get(i));
-				if(node.supportCount.get(i)>minsup*noOfTransactions){
-					TreeSet<Integer> temp = new TreeSet<Integer>(node.candidate.get(i));
-					TreeSet<Integer> temp1 = new TreeSet<Integer>(node.candidate.get(i));
-					double support = node.supportCount.get(i)/noOfTransactions;
-					RuleMining.itemsets.get(15-hash).add(temp1);
-					System.out.println("itemsets size: "+RuleMining.itemsets.get(15-hash).size()+" hash: "+this.hash);
-					RuleMining.itemWithSupport.put(temp,support);
-					status = true;
-				}
-			}
-		}*/
+				
 		return status;
-		//System.out.println("Hash value: "+this.hash+" Itemset size : "+itemsets.size());
 	}
 }
